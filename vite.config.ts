176c824ebb5ef3142c893c6d6d29c021c3790523
Harshal Lathiya <harshalplathiya@gmail.com>
2026-06-17 18:09:12 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/om-agri-inputs/', // GitHub repository name
+
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': '/src',
     },
   },
+
   server: {
     port: 5173,
     strictPort: true,
   },
 });
-
